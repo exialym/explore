bf86e736406d7c4616adf0f5c97a1944f920a3d4
exialym <exialym@icloud.com>
2017-06-29 21:18:54 +0800

    add article item

---
+++ yog2/home/client/components/Articles/ArticleItem.js
@@ -0,0 +1,37 @@
+/**
+ * Created by exialym on 2017/6/29.
+ */
+import * as React from 'react'
+import { Component, PropTypes } from 'react'
+import * as classnames from 'classnames'
+
+class ArticleItem extends Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+
+    }
+  }
+
+
+
+  render() {
+    const {article,actions} = this.props
+    return (
+      <divkey={article._id}>
+        <h1>{article.title}</h1>
+        <span>{article.detail}</span>
+        <button onClick={()=>actions.deleteArticle(article._id)}>delete</button>
+      </div>
+    )
+  }
+}
+
+ArticleItem.propTypes = {
+  // todo: PropTypes.object.isRequired,
+  // editTodo: PropTypes.func.isRequired,
+  // deleteTodo: PropTypes.func.isRequired,
+  // completeTodo: PropTypes.func.isRequired
+}
+
+export default ArticleItem
